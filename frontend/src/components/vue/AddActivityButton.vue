264c72483c1a84b669e8e4d16d7edb8c49fce8dd
Mitchell Puersten <mitch.puersten@gmail.com>
2023-06-28 14:47:26 +0000

    Added missing files.

---
+++ frontend/src/components/vue/AddActivityButton.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+function launchActivityModal() {
+    console.log("Launching modal.");
+}
+
+</script>
+
+<template>
+    <button @click="launchActivityModal">Add Activity</button>
+</template>
+
+<style>
+    button {
+        background: var(--secondary-background);
+        color: var(--text-color);
+        font-size: larger;
+        font-weight: bold;
+
+        padding: 12px;
+        border: none;
+        border-radius: 12px;
+    }
+
+    button:hover {
+        cursor: pointer;
+    }
+</style>
